1acdec02fc25b85935270eea21a38f417594a882
rudyvdtas <rudyvdtas@users.noreply.github.com>
2026-04-02 13:51:21 +0200

    Add Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,7 @@
-import adapter from '@sveltejs/adapter-node'
+import adapter from '@sveltejs/adapter-vercel'
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
+export default {
   kit: {
     adapter: adapter(),
   },
 }
-
-export default config
